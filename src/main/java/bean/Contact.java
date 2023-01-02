d9f11eedb9a6d204b07227da46514b5ad89c6206
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2023-01-02 20:18:51 +0700

    contact

--- src/main/java/bean/Contact.java
+++ src/main/java/bean/Contact.java
@@ -3,8 +3,8 @@ package bean;
 import java.io.Serializable;
 
 public class Contact implements Serializable {
-    private int contactID;
-    private int userID;
+    private String contactID;
+    private String userID;
     private String username;
     private String email;
     private String userSubject;
@@ -12,7 +12,7 @@ public class Contact implements Serializable {
     private String createDate;
     private String status;
 
-    public Contact(int contactID, int userID, String username, String email, String userSubject, String contactContent, String createDate, String status) {
+    public Contact(String contactID, String userID, String username, String email, String userSubject, String contactContent, String createDate, String status) {
         this.contactID = contactID;
         this.userID = userID;
         this.username = username;
@@ -23,35 +23,73 @@ public class Contact implements Serializable {
         this.status = status;
     }
 
-    public int getContactID() {
+    public Contact() {
+
+    }
+
+    public String getContactID() {
         return contactID;
     }
 
-    public int getUserID() {
+    public void setContactID(String contactID) {
+        this.contactID = contactID;
+    }
+
+    public String getUserID() {
         return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getUserSubject() {
         return userSubject;
     }
 
+    public void setUserSubject(String userSubject) {
+        this.userSubject = userSubject;
+    }
+
     public String getContactContent() {
         return contactContent;
+    }
+
+    public void setContactContent(String contactContent) {
+        this.contactContent = contactContent;
     }
 
     public String getCreateDate() {
         return createDate;
     }
 
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+
     public String getStatus() {
         return status;
     }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
