2b11d847dd2852b0d3ae6e5dc963612a68d46cdf
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2023-01-02 12:21:25 +0700

    logout,sendmail

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,13 +1,12 @@
 package service;
 
+import bean.Order;
 import bean.User;
-import bean.Product;
 import database.ConnectDB;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -23,12 +22,14 @@ public class UserService {
             listUsers = new LinkedList<>();
             while (rs.next()) {
                 listUsers.add(new User(
-                        rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                        rs.getString(1), rs.getString(2), rs.getString(3)
+                        , rs.getString(4),
                         rs.getInt(5),
                         rs.getString(6),
                         rs.getString(7),
                         rs.getInt(8),
-                        rs.getString(9)));
+                        rs.getString(9)
+                ));
             }
 
         } catch (ClassNotFoundException | SQLException e) {
@@ -36,6 +37,10 @@ public class UserService {
         }
 
         return listUsers;
+    }
+
+    public static List<Order> getAllProduct() {
+        return null;
     }
 
     public User getUser(String id) {
@@ -49,12 +54,14 @@ public class UserService {
             user = null;
             while (rs.first()) {
                 user = new User(
-                        rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                        rs.getString(1), rs.getString(2), rs.getString(3)
+                        , rs.getString(4),
                         rs.getInt(5),
                         rs.getString(6),
                         rs.getString(7),
                         rs.getInt(8),
-                        rs.getString(9));
+                        rs.getString(9)
+                );
             }
             rs.close();
             s.close();
@@ -72,8 +79,7 @@ public class UserService {
             s = ConnectDB.connect(sql);
             s.setString(1, uname);
             ResultSet rs = s.executeQuery();
-            if (rs.next())
-                return true;
+            if (rs.next()) return true;
             s.close();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -81,22 +87,23 @@ public class UserService {
         return false;
     }
 
+
     public static boolean register(User user) {
         PreparedStatement preSta = null;
         Random rd = new Random();
         try {
             String sql = "INSERT INTO user(id,username,email,password,role,name,phone,status,day_register) VALUES (?,?,?,?,?,?,?,?,?);";
             preSta = ConnectDB.connect(sql);
-            preSta.setString(1, "user" + rd.nextInt(1000000) + rd.nextInt(100000));
+            preSta.setString(1,"user"+rd.nextInt(1000000)+rd.nextInt(100000));
             preSta.setString(2, user.getUserName());
             preSta.setString(3, user.getEmail());
             preSta.setString(4, user.getPassWord());
             preSta.setInt(5, user.getIsAdmin());
             preSta.setString(6, user.getName());
-            preSta.setString(7, user.getPhone());
-            preSta.setInt(8, user.getStatus());
+            preSta.setString(7,user.getPhone());
+            preSta.setInt(8,user.getStatus());
             preSta.setString(9, user.getDay_register());
-            int rs = preSta.executeUpdate();
+            int rs=preSta.executeUpdate();
             preSta.close();
             return true;
         } catch (SQLException | ClassNotFoundException e) {
@@ -141,8 +148,10 @@ public class UserService {
             int rs = s.executeUpdate();
             s.close();
         } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
     }
+
 
     public static boolean existEmail(String email) {
         PreparedStatement s = null;
@@ -151,8 +160,7 @@ public class UserService {
             s = ConnectDB.connect(sql);
             s.setString(1, email);
             ResultSet rs = s.executeQuery();
-            if (rs.next())
-                return true;
+            if (rs.next()) return true;
             s.close();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -160,17 +168,16 @@ public class UserService {
         return false;
     }
 
-    public static boolean checkUsernameEmail(String userName, String email) {
+
+    public static boolean checkUsernameEmail(String email) {
         PreparedStatement preSta = null;
         try {
             String sql = "select username,email from user where username=? and email =?";
             preSta = ConnectDB.connect(sql);
-            preSta.setString(1, userName);
-            preSta.setString(2, email);
+            preSta.setString(1, email);
             ResultSet rs = preSta.executeQuery();
             if (rs.next()) {
                 rs.getString(2);
-                rs.getString(3);
                 return true;
             }
             rs.close();
@@ -180,6 +187,7 @@ public class UserService {
         }
         return false;
     }
+
 
     public int countUser() {
         PreparedStatement pre = null;
@@ -196,101 +204,29 @@ public class UserService {
         }
         return count;
     }
-
-    // Diem so luong product trong db
-    public int getCountProduct() {
-        PreparedStatement pre = null;
-        int count = 0;
-
+    public static void updatePassword(String email, String password) {
+        PreparedStatement preSta = null;
         try {
-            String sql = "select count(*) from products";
-            pre = ConnectDB.connect(sql);
-            ResultSet rs = pre.executeQuery();
-            while (rs.next()) {
-                return rs.getInt(1);
-            }
-        } catch (Exception e) {
+            String sql = "UPDATE user SET password=? WHERE email=? ";
+            preSta = ConnectDB.connect(sql);
+            preSta.setString(1, password);
+            preSta.setString(2, email);
+            preSta.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
         }
-        return 0;
-
     }
-
-    public static List<Product> paddingProduct(int index) {
-        List<Product> list = new ArrayList<>();
-        PreparedStatement pre = null;
-        try {
-            String sql = "Select * from products "
-                    + "order by id "
-                    + "offset ? rows fetch next 18 rows only";
-            pre = ConnectDB.connect(sql);
-            pre.setInt(1, (index - 1) * 18);
-            ResultSet rs = pre.executeQuery();
-            while (rs.next()) {
-                list.add(new Product(rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getString(5),
-                        rs.getInt(6),
-                        rs.getInt(7),
-                        rs.getString(8),
-                        rs.getString(9),
-                        rs.getInt(10),
-                        rs.getInt(11),
-                        rs.getInt(12),
-                        rs.getInt(13)));
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    // tìm kiếm theo tên sản phẩm
-    public static List<Product> searchByName(String txtSearch) {
-
-        PreparedStatement pre = null;
-        List<Product> list = new ArrayList<>();
-        String sql = "SELECT * FROM products WHERE name LIKE ?;";
-        try {
-            pre = ConnectDB.connect(sql);
-            pre.setString(1, "%" + txtSearch + "%");
-            ResultSet rs = pre.executeQuery();
-            while (rs.next()) {
-                list.add(new Product(rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getString(5),
-                        rs.getInt(6),
-                        rs.getInt(7),
-                        rs.getString(8),
-                        rs.getString(9),
-                        rs.getInt(10),
-                        rs.getInt(11),
-                        rs.getInt(12),
-                        rs.getInt(13)));
-            }
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-        return list;
-    }
-
     public static void main(String[] args) {
         UserService service = new UserService();
-        boolean u = existUserName("quanghuy.fs");
-        User user = new User("user1000000000", "quanghuy", "quanghuy0029a@gmail.com", "212002", 1, "Bùi Quang Huy",
-                "09878131", 1, "12/12/2002");
+//        boolean u = existUserName("quanghuy.fs");
+//        User user = new User("user1000000000","quanghuy","quanghuy0029a@gmail.com","212002",1,"Bùi Quang Huy","09878131",1,"12/12/2002");
 
-        System.out.println(u);
-        int count = service.getCountProduct();
-        System.out.println(count);
-        // System.out.println(register(user));
-        // UserService u = new UserService();
-        // System.out.println(u.register(new User("2","trung2", "trung2@gmail.com",
-        // "0912271440", "đl" , "1234")));
+//        System.out.println(u);
+//        System.out.println(register(user));
+//        UserService u = new UserService();
+//        System.out.println(u.register(new User("2","trung2", "trung2@gmail.com", "0912271440", "đl" , "1234")));
+        updatePassword("trung@gmail.com", "123123");
+        System.out.println(existEmail("trung@gmail.com"));
     }
 }
-
-//
